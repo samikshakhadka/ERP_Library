e21808af4b744fe6deddc30a22d358a8b1aeda8e
samikshakhadka <swabhimankhadka2001@gmail.com>
2024-09-10 21:31:24 +0545

    fix the display

--- frappe-bench/apps/library_management/library_management/library_management/doctype/article/article.py
+++ frappe-bench/apps/library_management/library_management/library_management/doctype/article/article.py
@@ -1,9 +1,15 @@
-# Copyright (c) 2024, sam and contributors
-# For license information, please see license.txt
+# # Copyright (c) 2024, sam and contributors
+# # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
 
 class Article(WebsiteGenerator):
 	pass
+
+
+# class Article(WebsiteGenerator):
+#     def before_save(self):
+#         if not self.route:
+#             self.route = f"/articles/{self.title.replace(' ', '-')}"
